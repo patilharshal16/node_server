d8a65a100277f11b94d75c869dd45a6117372a55
Harshal Patil <patilharshal4@gmail.com>
2020-05-25 15:23:32 +0530

    rename file names

--- httpsServer.js
+++ httpsServer.js
@@ -3,21 +3,10 @@ var cors = require('cors');
 var compression = require('compression');
 var path = require('path');
 var https = require('https');
-var http = require('http');
-var fs = require('fs');
 
 const app = express();
 
-const PORT = 443;
-
-const httpOptions = {
-  cert: fs.readFileSync(`/root/DONOTDELETE/genomesmart.com.cert`),
-  key: fs.readFileSync(`/root/DONOTDELETE/genomesmart.com.key`)
-};
-
 app.use(compression());
-
-var server = https.createServer(httpOptions, app);
 
 app.use(cors({
     origin: true,
@@ -27,27 +16,29 @@ app.use(cors({
     allowedHeaders: 'Cache-Control, Origin, Authorization, Content-Type, X-Requested-With',
 }));
 
-
-const DIST_FOLDER = path.join(process.cwd(), 'dist/browser/');
+const DIST_FOLDER = path.join(process.cwd(), 'dist');
 
 app.get('*', express.static(path.join(DIST_FOLDER, '')));
 app.get('*', (req, res) => {
     res.sendFile(path.join(DIST_FOLDER, '') + '/index.html');
 });
 
-
-app.use(express.static('dist/browser'));
+app.use(express.static('/dist'));
 
 app.get('*', function (req, res) {
-        res.setHeader("X-Frame-Options", "DENY");
-        res.sendFile('/dist/browser/index.html', { root: __dirname });
+    res.setHeader("X-Frame-Options", "DENY");
+    res.sendFile('/dist/index.html', { root: __dirname });
 })
 
-server.listen(PORT, function () {
-    console.log(`HTTPS server started listening on port:    ${PORT}`);
+app.listen(process.env.PORT || 4300, function () {
+    console.log("HTTP server started listening on port: %s", process.env.PORT || 4300);
 });
 
-http.createServer(function (req, res) {
-  res.writeHead(301, { 'Location': 'https://' + req.headers['host'] + req.url });
-  res.end();
-}).listen(80);
+const options = {
+   cert: fs.readFileSync(`certificate.pem`),
+   key: fs.readFileSync(`privateKey.pem`)
+};
+
+https.createServer(options, app).listen(config.PORT, () => {
+   console.log(`HTTPS server started listening on port:    ${config.PORT}`);
+});
